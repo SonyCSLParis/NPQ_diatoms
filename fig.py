2ce7df69c2b26ba4fd70c90d5c8dd356c2103207
kodda <koddda@gmail.com>
2022-06-14 22:20:51 +0200

    add fig

--- fig.py
+++ fig.py
@@ -12,13 +12,12 @@ ke_params = {"A": 1, "alpha": 0.5, "I1": 100}
 kd_params = {"A1": 0.05, "A2": .5, "I50NPQ": 200, "n": 4}
 
 Is =np.linspace(0,500,100)
-NPQs = NPQ(Is, ke_params, kd_params, LHCX)
+NPQs = dg.NPQ(Is, ke_params, kd_params, LHCX)
 
-ETRs   = ETR(Is, ETR_max, Ek)
-phiIIs = phiII(Is, ETRs, sII)
-Fmps = Fmp(NPQs, Fm)
-Fps = Fp(phiIIs, Fmps)
-
+ETRs   = dg.ETR(Is, ETR_max, Ek)
+phiIIs = dg.phiII(Is, ETRs, sII)
+Fmps = dg.Fmp(NPQs, Fm)
+Fps = dg.Fp(phiIIs, Fmps)
 
 pl.subplot(221)
 pl.plot(Is, ETRs)
